24cb80795d9098ae4cc28f5913d8c784566bd5e3
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2022-10-15 12:09:40 -0400

    updated pdp components

---
+++ components/ProductDetails.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { FC } from "react";
+
+const ProductDetails: FC = () => {
+  return <section className="product"></section>;
+};
+
+export default ProductDetails;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import ProductDetails from "components/ProductDetail";
+import ProductDetails from "components/ProductDetails";
 
 const PDP: NextPage = () => (
   <div className="container">
