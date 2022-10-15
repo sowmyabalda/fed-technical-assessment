8512ede127db2400236cc1c5d882db45fd56724c
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2022-10-15 04:17:02 -0400

    renamed css file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import "../styles/home.css";
+import "../styles/product-page.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
